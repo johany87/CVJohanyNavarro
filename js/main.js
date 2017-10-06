05226e7573b35b0f2072fdc07b95947f28a4c1c5
Ing. Johany <johany87@gmail.com>
2017-10-06 16:20:23 -0500

    add english button
    add english button

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
-
   function blackcolor(img) {
     img.style.color = "black";
   }
-
   function orangecolor(img) {
     img.style.color = "#f0563d";
   }
-
   function shadowin(div) {
-    div.style = "box-shadow: 0px 0px 30px 0px rgba(130, 130, 130, 0.75);";
+    div.style = "box-shadow: 0px 0px 30px 0px rgba(130, 130, 130, 0.75)";
   }
-
   function shadowout(div) {
     div.style = "box-shadow: 0px 0px 0px 0px";
   }
